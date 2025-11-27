5718befdec6d71b84ac749ecd20f3a48b2f292c0
Anass Chadli <kinganass2004@gmail.com>
2025-11-27 16:20:11 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright by the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+module wallettemplate {
+    requires java.desktop;
+    requires java.logging;
+
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.jspecify;
+
+    requires org.slf4j;
+    requires com.google.common;
+    requires com.google.zxing;
+
+    requires org.bitcoinj.core;
+    requires  com.google.protobuf;
+
+    exports wallettemplate;
+    exports com.anass.wallet.controls;
+    opens wallettemplate to javafx.fxml;
+    opens com.anass.wallet.utils to javafx.fxml;
+    opens com.anass.wallet.controls to javafx.fxml;
+}
